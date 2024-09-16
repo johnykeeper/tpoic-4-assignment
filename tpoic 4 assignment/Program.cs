9d4a8cf1e6d8909a7c8d5e5eb1623e65b8f31ec0
Jonathan Gaboury <362568651@GoTVDSB.ca>
2024-09-16 09:32:42 -0400

    Add project files.

---
+++ tpoic 4 assignment/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace tpoic_4_assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Jonathan
+            string name;
+            int age;
+            double money;
+
+
+            Console.WriteLine("hello. what is your name");
+            name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("press Enter to continue");
+            Console.ReadLine();
+            Console.WriteLine("Hi, " + name + " How old are you?");
+            int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine();
+            Console.WriteLine("oh wow you're only " + age + " how much money do you make a hour?");
+            double.TryParse(Console.ReadLine(), out money);
+        }
+    }
+}
